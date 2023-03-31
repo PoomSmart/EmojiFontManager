5177c3bd67f9656e54958e369e0bedd2ca9bc900
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2023-03-31 21:00:34 +0700

    Update Prefs.h

--- Prefs.h
+++ Prefs.h
@@ -4,5 +4,5 @@
 #define domain CFSTR("com.apple.UIKit")
 #define selectedFontKey CFSTR("selectedEmojiFont")
 #define defaultName @"Default"
-#define fontsPath ROOT_PATH_NS(@"/Library/Themes/EmojiFontManager")
+#define fontsPath ROOT_PATH_NS("/Library/Themes/EmojiFontManager")
 #define tweakIdentifier domain
